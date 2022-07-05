5b0b9e057adf8fae8ea9a37a0d7e9992a2ffc1b4
Boi Schrader <boi1992@t-online.de>
2022-07-05 13:42:48 +0200

    language pitfalls done

--- solution.js
+++ solution.js
@@ -1,1 +1,20 @@
+console.log('---------------------------')
 
+console.log('1.')
+const messageOne = 'Hello'
+const numberOne = '3'
+console.log('messageOne is -', parseInt(messageOne))
+console.log('numberOne is -', parseInt(numberOne))
+
+console.log('---------------------------')
+
+console.log('2.')
+console.log(0.1 * 0.2);
+const rounding = (0.1 * 0.2)
+console.log((0.1 * 0.2).toFixed(2));
+
+console.log('---------------------------')
+console.log('3.')
+console.log(Infinity / 0)
+console.log(Infinity / Infinity)
+console.log(1/0)
